437cc3a8199f362b78eac06281046ce68ad3892c
Bruno Leonardo Michels <brunolm7@gmail.com>
2016-12-17 13:46:52 -0200

    Heroku port from env

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.use('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, '0.0.0.0', () => {
+app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
   console.log('Started!');
 });
